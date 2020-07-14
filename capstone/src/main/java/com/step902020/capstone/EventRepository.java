4efb36900adeb9b25afa10aa28ae4267dc9ea00f
lbourret <lbourret@google.com>
2020-07-14 14:25:57 -0400

    Fix merge conflicts

--- capstone/src/main/java/com/step902020/capstone/EventRepository.java
+++ capstone/src/main/java/com/step902020/capstone/EventRepository.java
@@ -5,5 +5,8 @@ import org.springframework.cloud.gcp.data.datastore.repository.DatastoreReposito
  * Spring Data Repository for Event Entities
  */
 public interface EventRepository extends DatastoreRepository<Event, Long> {
-
+    @Query("select * from event where requiredFood  @name and name < @endname and university = @university")
+    public List<Organization> findEventsByFilters(
+            @Param("foodAvailable") List<Boolean> food,
+            @Param("requiredFee") List<Boolean> fee);
 }
